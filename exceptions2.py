eecaba6e23359f0cd9978fe9f9332e82c0595913
JAYARAJ PEEDIYAKAL SIVADASAN <jayaraj.sivadasan@gmail.com>
2021-04-17 18:36:53 -0400

    Create exceptions2.py

---
+++ exceptions2.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+
+try:
+    file = open("app.py")
+    pprint(file.readlines())
+    age = int(input("Age"))
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age")
+else:
+    print("THere was no errors")
+finally:
+    file.close() #finally block closes the open file
+print("Execution continues...")

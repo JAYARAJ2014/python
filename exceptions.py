2f94d7edb7478c5a383ef188f7789aa3371dc392
JAYARAJ PEEDIYAKAL SIVADASAN <jayaraj.sivadasan@gmail.com>
2021-04-18 10:25:48 -0400

    Exception Handling Example

--- exceptions.py
+++ exceptions.py
@@ -3,7 +3,7 @@
 try:
     age = int(input("Age"))
     xfactor = 10/age
-except (ValueError, ZeroDivisionError):
+except (ValueError, ZeroDivisionError): #Multiple exceptions in oneline.
     print("You didn't enter a valid age")
 else:
     print("THere was no errors")

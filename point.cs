2f129d857ef63a68f96056b8bc10ca5615c0963e
JAYARAJ PEEDIYAKAL SIVADASAN <jayaraj.sivadasan@gmail.com>
2021-04-17 20:20:41 -0400

    Update point.cs

--- point.cs
+++ point.cs
@@ -1,4 +1,6 @@
 class Point:
+    default_color = "green"  # like static
+
     def __init__(self, x, y):  # method should have at least one parameter self
         self.x = x
         self.y = y
@@ -6,6 +8,15 @@ class Point:
     def draw(self):
         print(f"Point({self.x}, {self.y})")
 
+    @classmethod  # class level method
+    def zero(cls):
+        return cls(0, 0)
 
-point = Point(4, 5)
+
+point = Point.zero()
 point.draw()
+point.z = 90
+
+Point.default_color = "yellow"
+print(point.default_color)
+print(point.default_color)

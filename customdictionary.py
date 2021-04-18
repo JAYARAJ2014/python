9ab377340bd0044fdc810f983ea9b12dca1bdcad
JAYARAJ PEEDIYAKAL SIVADASAN <jayaraj.sivadasan@gmail.com>
2021-04-18 10:27:22 -0400

    Custom Dictionary & Magic Methods

--- customdictionary.py
+++ customdictionary.py
@@ -1,12 +1,12 @@
 class TagCloud:
-    def __init__(self):
+    def __init__(self):  ## __init__ is constructor.
         self.__tags = {}  # __ is treated like private
 
     def add(self, tag):
         self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0)+1
 
-    def __getitem__(self, tag):
-        return self.__tags.get(tag.lower(), 0)
+    def __getitem__(self, tag):  # __getitem__ is used so that you can access the element using square brackets as in Line 25
+        return self.__tags.get(tag.lower(), 0) 
 
     def __setitem__(self, tag, count):
         self.__tags[tag.lower] = count
